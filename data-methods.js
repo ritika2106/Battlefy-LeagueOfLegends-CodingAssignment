a6f2b9d60e91400c6a25831d1c222b1899c4945b
Ritika Kaushal <rnkaushal@myseneca.ca>
2022-09-19 08:47:55 -0400

    server with sample get and post

---
+++ data-methods.js
@@ -0,0 +1,134 @@
+process.env.LEAGUE_API_KEY = "RGAPI-78d45603-2beb-4d0a-8648-4c03be9c22b6"
+const LeagueJS = require('./node_modules/leaguejs/lib/LeagueJS.js');
+const leagueJs = new LeagueJS(process.env.LEAGUE_API_KEY);
+
+//summoner name made accessible to all methods to prevent 
+//passing it separately to each method
+let summoner = ""
+
+module.exports = {
+
+    /*
+    getting encrypted summonerId and puuid 
+    using summoner name
+    */
+
+    getIdByName(summonerName) {
+        return new Promise((resolve, reject) => {
+            leagueJs.Summoner
+                .gettingByName(summonerName)
+                .then(data => {
+                    summoner = summonerName;
+                    return resolve(data);
+                })
+                .catch(err => {
+                    console.log("Data could not be fetched, API failed with error: ", err);
+                    return reject(err);
+                });
+
+        })
+    },
+
+    /*
+    getting an array of all matchIds that were played
+    by the summoner using puuid of the summoner
+    */
+
+    getAllMatchesPlayedBySummoner(puuid) {
+        return new Promise((resolve, reject) => {
+            leagueJs.Match.gettingMatchIdsByPuuid(puuid)
+                .then((data) => {
+                    return resolve(data);
+                })
+                .catch((err) => {
+                    console.log("Data could not be fetched, API failed with error: ", err);
+                    return reject(err)
+                })
+
+        })
+
+    },
+
+    /*
+    loops through an array of participants per match id
+    and finds the participant that matches the summoner name
+    then stores all the data in one final object
+    */
+
+    getMatchDataPerSummonerMatch(matchId) {
+        return new Promise((resolve, reject) => {
+            let participantDetail = {};
+            if(matchId && Object.keys(matchId).length > 0){
+            leagueJs.Match.gettingById(matchId)
+                .then((data) => {
+                    participantDetail.gameDuration = data.info.gameDuration;
+                    participantDetail.daysAgo = Math.floor(((data.info.gameEndTimestamp / 1000 / 60 / 60) % 24) / 24);
+
+                    data.info['participants'].forEach(el => {
+                        if (el.summonerName === summoner) {
+                            participantDetail.victory = (el.win) ? "VICTORY" : "DEFEAT"
+                            participantDetail.summonerName = el.summonerName;
+                            participantDetail.championName = el.championName;
+                            participantDetail.champLevel = el.champLevel;
+                            participantDetail.kills = el.kills;
+                            participantDetail.deaths = el.deaths;
+                            participantDetail.assists = el.assists;
+                            participantDetail.totalMinionsKilled = el.totalMinionsKilled;
+                            participantDetail.kda = el.challenges.kda;
+                            participantDetail.items = [];
+                            for (let i = 0; i < 7; i++) {
+                                let itemNum = "item" + i;
+                                if (el[itemNum] != 0)
+                                    participantDetail.items.push(el[itemNum])
+                            }
+                            participantDetail.spells = [];
+                            for (let i = 1; i <= 4; i++) {
+                                let spell = "spell" + i + "Casts";
+                                participantDetail.spells.push(el[spell]);
+                            }
+                            participantDetail.perks = el.perks.statPerks;
+                        }
+                    })
+                    return resolve(participantDetail)
+                })
+                .catch((err) => {
+                    console.log("No data retreived, API failed with error: ", err);
+                    return reject(err);
+                })
+            }
+            else{
+                return reject("No Match Id received by the method")
+            }
+        })
+    },
+
+    /*
+    based on the number of matches to loop through, the method
+    loops through the decided number and passes each one to 
+    "getMatchDataPerSummonerMatch" to retreive summoner 
+    specific stats and pushes acquired to an array after each iteration
+    */
+    getAllMatchesSummonerDetails(allMatchIds) {
+        return new Promise(async (resolve, reject) => {
+            let numOfMatches = (Object.keys(allMatchIds).length) > 5 ? 5 : (Object.keys(allMatchIds).length)
+            let allMatchDataArr = [];
+            if (numOfMatches && numOfMatches > 0) {
+                for (let i = 0; i < numOfMatches; i++) {
+                    await this.getMatchDataPerSummonerMatch(allMatchIds[i])
+                        .then((matchDataObj) => {
+                            allMatchDataArr.push(matchDataObj);
+                        })
+                        .catch((err) => {
+                            console.log("No data retreived, API failed with error: ", err)
+                            return reject(err);
+                        })
+                }
+                return resolve(allMatchDataArr);
+            }
+            else {
+                return reject("No matches were played by the summoner, no matchIds received");
+            }
+        })
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,52 @@
+const dataMethods = require("./data-methods.js");
+const express = require("express");
+const cors = require("cors");
+var bodyParser = require("body-parser")
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log("Server running and listening for requests!");
+})
+
+app.get('/read', (req, res) => {
+    res.send({ read: "Go to the home page to enter your summoner name and view your LoL game statistics!"});
+})
+
+app.post('/summoner', (req, res) => {
+    //console.log("node request body: ", req.body);
+    if(req.body && req.body.sumName){
+    dataMethods.getIdByName(req.body.sumName)
+        .then((summonerDetails) => {
+            dataMethods.getAllMatchesPlayedBySummoner(summonerDetails.puuid)
+                .then((allMatches) => {
+                    dataMethods.getAllMatchesSummonerDetails(allMatches)
+                        .then((allMatchesSummonerData) => {
+                            res.send(allMatchesSummonerData);
+                        })
+                        .catch((err) => {
+                            console.log("getAllMatchesSummonerDetails API failed with error: ", err);
+                            res.send("API failed with error: ", err);
+                        })
+                })
+                .catch((err) => {
+                    console.log("getAllMatchesPlayedBySummoner API failed with error: ", err);
+                    res.send("API failed with error: ", err);
+                })
+        })
+        .catch((err) => {
+            console.log("getIdByName API failed with error: ", err);
+            res.send("API failed with error: ", err);
+        })
+    }
+    else{
+        res.send("Empty body received. Cannot process any further")
+    }
+})
+
